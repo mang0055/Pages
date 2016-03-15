0a3f4553490fb12e06eca4f939f6d71006705f02
Raviraj <mang0055@algonquinlive.com>
2016-03-15 13:48:07 -0400

    let test

--- mad9022/demo/www/js/index.js
+++ mad9022/demo/www/js/index.js
@@ -16,38 +16,20 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        console.log('DeviceID: '+device.uuid);
-    }
-};
+// var app = {
+//     // Application Constructor
+//     initialize: function() {
+//         this.bindEvents();
+//     },
+//     // Bind Event Listeners
+//     //
+//     // Bind any events that are required on startup. Common events are:
+//     // 'load', 'deviceready', 'offline', and 'online'.
+//     bindEvents: function() {
+//         document.addEventListener('deviceready', init, false);
+//     }
+// };
+document.addEventListener("DOMContentLoaded", init);
 function init(ev) {
 
     var pl = document.querySelectorAll(".page-link");
@@ -106,4 +88,4 @@ function pageAnimated(ev) {
         ev.target.classList.add("hidden");
     }
 }
-app.initialize();
+//app.initialize();
